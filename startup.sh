101bbdff0e65f7965f607042b966f5d0f09be7c7
Bill Clifford <bcliffor@systemsbiology.org>
2017-12-12 11:06:41 -0800

    Adding HTTPS support

--- startup.sh
+++ startup.sh
@@ -2,5 +2,15 @@
 # This is script is called after the initial reboot of a camic-viewer VM.
 # We want to execute run_viewer.sh as the same user that instantiated the VM.
 
+set -x
+
+PROGNAME=$(basename "$0")
+
+if [ "$#" -ne 4 ]; then
+    echo "Usage: ./$PROGNAME <quip-viewer version> <admin email> <ip addr> <server alias>"
+    exit 1;
+fi
+
 cd /home/cvproc/quip_distro
-./run_viewer.sh $1
+
+./run_viewer.sh $1 $2 $3 $4
