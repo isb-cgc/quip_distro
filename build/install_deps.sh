85340741241e19c0e7887bbd3a75c920c83e72f1
Bill Clifford <bcliffor@systemsbiology.org>
2018-05-11 10:24:58 -0700

    Include full path to freshclam.conf

--- build/install_deps.sh
+++ build/install_deps.sh
@@ -54,7 +54,7 @@ sudo apt install -y clamav clamav-daemon
 wget https://raw.githubusercontent.com/isb-cgc/ISB-CGC-Cron/master/gce_vm_tasks/virus_scan.sh?token=AIDOy_cOcMmcva217njQkMtrB3alVbrRks5ay64awA%3D%3D -O virus_scan
 chmod 0755 virus_scan
 sudo cp virus_scan /etc/cron.daily/
-sudo sed -ie 's/Checks 24/Checks 2/' freshclam.conf
+sudo sed -ie 's/Checks 24/Checks 2/' /etc/clamav/freshclam.conf
 
 ### Do the update/upgrade thing
 sudo apt-get -y update
