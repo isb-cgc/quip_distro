944bda2759ad69de2f66758669a0ac5e40c80d36
William Clifford <bcliffor@systemsbiology.org>
2018-10-17 23:13:05 +0000

    Get a real cert

--- build/install_nginx.sh
+++ build/install_nginx.sh
@@ -28,7 +28,7 @@ sudo rm ir_addr.txt
 
 # Then we create a new cert. Note that Let's Encrypt strictly limits creating new certs on the
 # same domain to 10 in a one week period. 
-sudo certbot --nginx -m $SERVER_ADMIN -d $MACHINE_URL --redirect --agree-tos --non-interactive --staging
+sudo certbot --nginx -m $SERVER_ADMIN -d $MACHINE_URL --redirect --agree-tos --non-interactive
 
 # Edit the letsencrypt config file so as to not enable TLS v1.0
 sudo sed -ie 's/TLSv1 / /' /etc/letsencrypt/options-ssl-nginx.conf
@@ -36,26 +36,4 @@ sudo sed -ie 's/TLSv1 / /' /etc/letsencrypt/options-ssl-nginx.conf
 # Restart nginx
 sudo nginx -s stop
 sudo nginx
-
-
-#CERTS=`sudo certbot certificates`
-
-### See if we already have a valid certificate
-#if [[ $CERTS == "" ]] || [[ $CERTS== *"INVALID"* ]] || [[ $CERTS != *"camic-viewer-dev.isb-cgc.org"* ]]; 
-#then 
-    # No cert or an invalid certificate for this subdomain exists. We assume that nginx.conf has been configured by
-    # letsencrypt/certbot.
-    # Delete the invalid certificate
-#    sudo certbot delete
-    # Get a valid certificate for this VM. It will expire in 90 days, but certbot should renew it.
-#    sudo certbot --nginx -m $SERVER_ADMIN -d $MACHINE_URL --redirect --agree-tos --non-interactive
-#else
-    # A valid certificate for this subdomain exists. However, since this is a new VM, we need certbot to configure nginx.conf.
-    # So we get an invalid certificate for this VM. This will have the side effect of fixing the nginx.conf as needed.
-#    sudo certbot --nginx -m $SERVER_ADMIN -d $MACHINE_URL --redirect --agree-tos --non-interactive --staging
-    # Now we can delete the invalid cert and refresh with a valid cert.
-    ## sudo cerbot delete
-    # Then, we tell certbot to renew the certificate. This should install a "real" cert.
-#    sudo certbot renew
-#fi
 
